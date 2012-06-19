eac7b63acad4dbf7e6b2665888b5540942d7f4ea
mansol <esolvetex@yahoo.com>
2012-06-19 15:40:38 -0500

    Update master

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,10 +1,13 @@
 # Add a declarative step here for populating the DB with movies.
 
-Given /the following movies exist/ do |movies_table|
-  movies_table.hashes.each do |movie|
+#Given /the following movies exist/ do |movies_table|
+#  movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    
+Given /^(?:|I )check the following ratings:(.*) $/ do |values| 
+  values.split(",").each do |rating| And %Q{I check "ratings" + "#{rating}} 
+#end end
+     
    Movie.create!(movie) 
   end
  # flunk "Unimplemented"
